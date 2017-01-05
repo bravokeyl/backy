29bbe0090222df36bc5490d16d97e90ae9649f7d
bravokeyl <bravokeyl@gmail.com>
2017-01-05 15:33:51 +0530

    image opacity

--- header.php
+++ header.php
@@ -20,14 +20,15 @@
 	if ( has_post_thumbnail() && ( is_single() || ( is_page() && !is_front_page() ) ) ) {
 		$featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
 		$featured_img = $featured_image[0];
+	  $fea_style =  'background: linear-gradient(rgba(0,0,0,.15),rgba(0,0,0,.15)),url('.$featured_img.') center center';
 	} else {
-		$featured_img = "";
+		$fea_style = "";
 	}
 	?>
 	<?php if(is_front_page()) {
 		get_template_part( 'template-parts/slider' );
 	} else { ?>
-	<div class="hero" style="background-image:url(<?php echo $featured_img; ?>);">
+	<div class="hero" style="<?php echo $fea_style;?>" >
     <div class="container">
     	<div class="row">
     		<div class="col-sm-8">
