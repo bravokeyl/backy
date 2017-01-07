9503aaaff8cb515b22fc1caa9935a892da7fe566
bravokeyl <bravokeyl@gmail.com>
2017-01-07 14:30:22 +0530

    Add dropdown icon

--- assets/js/bk.js
+++ assets/js/bk.js
@@ -1,12 +1,10 @@
 (function( $ ) {
   var topmenuparent = $('.top-menu .menu-item-has-children');
   topmenuparent.on('mouseenter',function(){
-    console.log($(this).find('.sub-menu'));
     $(this).addClass('open');
     // $(this).find('.sub-menu').show();
   });
   topmenuparent.on('mouseleave',function(){
-    console.log("Mouse Leaving");
     // $(this).find('.sub-menu').hide();
     $(this).removeClass('open');
   });
