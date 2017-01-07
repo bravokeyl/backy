6e6b19bdb09cb857447676c72add95b03fb11390
bravokeyl <bravokeyl@gmail.com>
2017-01-07 14:41:17 +0530

    Add dropdown icon

--- assets/js/bk.js
+++ assets/js/bk.js
@@ -1,6 +1,15 @@
 (function( $ ) {
   var topmenuparent = $('.top-menu .menu-item-has-children');
-  topmenuparent.on('click',function(){
+  topmenuparent.on('click',function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    $(document).one('click', function closeMenu (e){
+        if(topmenuparent.has(e.target).length === 0){
+            topmenuparent.removeClass('on');
+        } else {
+            $(document).one('click', closeMenu);
+        }
+    });
     $(this).toggleClass('on');
   });
 })( jQuery );
