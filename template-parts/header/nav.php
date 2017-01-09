eebc6e3c67e591bb777b794ac75d00b31e32bf6b
bravokeyl <bravokeyl@gmail.com>
2017-01-09 18:52:42 +0530

    custom logo:

--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -9,10 +9,19 @@
         <li><a href="tel:<?php echo esc_attr($pno);?>"><i class="fa fa-phone"></i><span class="number"><?php echo $pno;?></span></a></li>
         <li><a href="mailto:<?php echo esc_attr($email);?>"><i class="fa fa-paper-plane"></i><?php echo get_theme_mod( 'contact_email' );?></a></li>
       </ul>
-      <a class="navbar-brand" href="/">
-      <img src="/wp-content/themes/identity-experts/img/logo.png" alt="Identity Experts Logo">
-      </a>
-
+      <?php
+      $custom_logo_id = get_theme_mod( 'custom_logo' );
+      if ( $custom_logo_id ) {
+          $html = sprintf( '<a href="%1$s" class="navbar-brand custom-logo-link" >%2$s</a>',
+              esc_url( home_url( '/' ) ),
+              wp_get_attachment_image( $custom_logo_id, 'full', false, array(
+                  'class'    => 'custom-logo',
+                  'itemprop' => 'logo',
+              ) )
+          );
+      }
+      echo $html;
+      ?>
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-1">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
